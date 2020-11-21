07421d1e865987114d224bff64cdabab0f00ce07
Maximillian Kirsch <maximillian.kirsch@alumni.fh-aachen.de>
2020-11-21 12:15:51 +0100

    C++ library package with an include folder

---
+++ academy_lib/include/academy_lib/colors.h
@@ -0,0 +1,13 @@
+#ifndef ACADEMY_LIB_COLORS_H
+#define ACADEMY_LIB_COLORS_H
+
+#include <std_msgs/msg/color_rgba.hpp>
+
+
+namespace academy {
+
+std_msgs::msg::ColorRGBA getColor(double value);
+
+};
+
+#endif // ACADEMY_LIB_COLORS_H
