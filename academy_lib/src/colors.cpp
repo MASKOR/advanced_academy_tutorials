3d673800524f6f057d0636186d6e3dd1064b8ca3
Maximillian Kirsch <maximillian.kirsch@alumni.fh-aachen.de>
2020-12-19 17:24:13 +0100

    copyright

--- academy_lib/src/colors.cpp
+++ academy_lib/src/colors.cpp
@@ -1,3 +1,12 @@
+/* Copyright (C) 1883 Thomas Edison - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the XYZ license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the XYZ license with
+ * this file. If not, please write to: , or visit :
+ * (Linting example)
+ */
 #include "academy_lib/colors.h"
 
 #include <math.h>
@@ -7,10 +16,11 @@
 #include <angles/angles.h>
 
 #include <iostream>
-double value;
+double buf[10];
 std_msgs::msg::ColorRGBA academy::getColor(double value)
 {
   int tmp = 0;
+  buf[int(value)] = 0;
   double degrees = angles::to_degrees(angles::normalize_angle_positive(value));
   cv::Mat3f hsv(cv::Vec3f(degrees, 1.0, 1.0));
   cv::Mat3f bgr;
